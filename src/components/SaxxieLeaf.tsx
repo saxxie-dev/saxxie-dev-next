bbe81a9172e5bb1f8002d93aa676650dc440bbec
saxxie-dev <c@saxxie.dev>
2024-07-14 21:50:25 -0500

    Make a pretty logo thingy

---
+++ src/components/SaxxieLeaf.tsx
@@ -0,0 +1,16 @@
+export function SaxxieLeaf() {
+  return (
+    <div
+      role="img"
+      className="min-height-72 bg-secondary relative aspect-square rounded-xl rounded-bl-3xl rounded-tr-[90%]"
+    >
+      <div className="from-primary-600 to-secondary shadow-background size-full rounded-xl rounded-bl-[90%] rounded-tr-[90%] bg-gradient-to-tl shadow-2xl" />
+      <div className="from-primary to-secondary absolute right-0 top-[29.7%] size-[15.2%] rounded-full bg-gradient-to-br" />
+      <div className="from-primary to-secondary-200 absolute right-0 top-[44.5%] size-[9.3%] rounded-full bg-gradient-to-br" />
+      <div className="from-primary to-secondary-300 absolute right-0 top-[53.7%] size-[6.1%] rounded-full bg-gradient-to-br" />
+      <div className="from-primary to-secondary-400 absolute right-0 top-[59.8%] size-[4.3%] rounded-full bg-gradient-to-br" />
+
+      <div className="from-accent-300 to-accent absolute right-0 top-0 size-[30.9%] origin-bottom-left rounded-[50%] rounded-tr-xl bg-gradient-to-tl" />
+    </div>
+  );
+}
